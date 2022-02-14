08926991889bd080edd2e2ae9073698b5bc846da
Larisa Gheorghe <larisa.camelia.gheorghe@gmail.com>
2022-02-14 21:07:19 +0200

    create header

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-transparent">
+    <a class="navbar-brand" href="#">Larisa &#38; Alexandru</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse"
+      data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+      aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbar">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Despre <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Oamenii de suflet</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Evenimentele</a>
+        </li>
+      </ul>
+    </div>
+
+  </nav>
+</template>
