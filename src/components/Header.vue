0037104e8cdb625897574e8fdf821071736ec953
Larisa Gheorghe <larisa.camelia.gheorghe@gmail.com>
2022-02-16 21:21:05 +0200

    resolve toggle navigation

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-transparent fixed-top">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
     <a class="navbar-brand" href="#">Larisa &#38; Alexandru</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse"
       data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
@@ -7,7 +7,7 @@
       <span class="navbar-toggler-icon"></span>
     </button>
 
-    <div class="collapse navbar-collapse" id="navbar">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ml-auto">
         <li class="nav-item">
           <a class="nav-link" href="#despre-section">
